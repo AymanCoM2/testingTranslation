a46d6791c971666debb966a6ed8a6998a62f8bbf
Ayman <ayman.salah@2coom.com>
2024-09-08 12:58:53 +0300

    g

--- routes/web.php
+++ routes/web.php
@@ -3,31 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
-
+ini_set('max_execution_time', 3600); // 3600 seconds = 60 minutes
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/test-translate', function () {
-    $tr = new GoogleTranslate(); // Translates to 'en' from auto-detected language by default
-    $tr->setSource('en'); // Translate from English
-    $tr->setSource(); // Detect language automatically
-    $tr->setTarget('ar'); // Translate to Georgian
-
-    // $result   = $tr->translate('search_by_brand_name');
-    // $result   = $tr->translate('search by brand name');
-    // $result   = $tr->translate('don’t_show_again');
-    // $result   = $tr->translate('https://console.firebase.google.com/');
-    // $result   = $tr->translate(')');
-    // $result   = $tr->translate('N/A');
-    // $result   = $tr->translate('ex_:_1216_Dhaka');
-    // $result   = $tr->translate('for_“Ready_Product”_deliveries _customers_can_pay_&_instantly_download_pre-uploaded_digital_products._For_“Ready_After_Sale”_deliveries _customers_pay_first_then_vendor_uploads_the_digital_products_that_become_available_to_customers_for_download');
-    $result   = $tr->translate('');
-
-    dd($result);
-})->name('test');
-
 
 
 /**
@@ -45,44 +24,41 @@ Route::get('/test-translate', function () {
  * 5- Big Messages will lot of Special Characters are not Working 
  * 6-
  */
+Route::get('/test-translate', function () {
+
+    // $result   = $tr->translate('search_by_brand_name');
+    // $result   = $tr->translate('search by brand name');
+    // $result   = $tr->translate('don’t_show_again');
+    // $result   = $tr->translate('https://console.firebase.google.com/');
+    // $result   = $tr->translate(')');
+    // $result   = $tr->translate('N/A');
+    // $result   = $tr->translate('ex_:_1216_Dhaka');
+    // $result   = $tr->translate('for_“Ready_Product”_deliveries _customers_can_pay_&_instantly_download_pre-uploaded_digital_products._For_“Ready_After_Sale”_deliveries _customers_pay_first_then_vendor_uploads_the_digital_products_that_become_available_to_customers_for_download');
+    // $result   = $tr->translate('');
+
+    // dd($result);
+})->name('test');
 
 
-/**
- * The Returning Method : 
- *  - Either Saving it as array 
- *  - Writing to the File itself 
- * 
- * 
- * 1- Take Array as a Real Return 
- * 2- Make new Array With translation 
- * Write to New File / Or same File -> + The Words [ <?php return + Dump the array ]
- */
 
-function textCleaner()
-{
-    /**
-     * This is Supposed to Clean the Text From any Special Characters 
-     * and also Keep the Original One 
-     * 
-     */
-    $testArray  = [
-        'text' => 'translation'
-    ];
 
-    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-    $txt = "John Doe\n";
-    fwrite($myfile, $txt);
-    $txt = "Jane Doe\n";
-    fwrite($myfile, $txt);
-    fclose($myfile);
-}
+
+
 
 
 Route::get('/write', function () {
-    $testArray  = [
-        'text' => 'translation'
-    ];
+    $tr = new GoogleTranslate(); // Translates to 'en' from auto-detected language by default
+    $tr->setSource('en'); // Translate from English
+    $tr->setSource(); // Detect language automatically
+    $tr->setTarget('ar'); // Translate to Georgian
 
+    // $messagesArray  = include 'messages.php';
+    // $newTranslatedArray  =  [];
+
+    // foreach ($messagesArray as $key => $value) {
+    //     $result   = $tr->translate($key);
+    //     $newTranslatedArray[$key] = $result;
+    // }
     // a:1:{s:4:"text";s:11:"translation";}
     // $myfile = fopen("../routes/newfile.php", "w") or die("Unable to open file!");
     // $txt = "<?php return  \n";
@@ -93,15 +69,31 @@ Route::get('/write', function () {
     // file_put_contents("../routes/newfile.php", print_r("<?php return  \n", true));
 
     // file_put_contents("../routes/newfile.php", "<?php return ".var_export($testArray, true).";");
-    $messagesArray  = include 'messages.php' ; 
 
-    dd($messagesArray);
-    
+    // file_put_contents("../routes/newfile.php", "<?php return " . var_export($newTranslatedArray, true) . ";");
+
+    $result   = $tr->translate('set the default commission amount that will be received from vendors on each order');
+    dd($result);
 })->name('write');
 
 /**
  * Two problems : 
  *      1- The Initial Translation For the Project 
+ *          - I think This is Ok ..
  *      2- The 'appending' For any new Words after that 
+ *          Scenration - Append : ??
  *      3- 
+ */
+
+
+/**
+ * Anomalies : 
+    // -  'company@gmail.com' => 'شركة@gmail.com',
+    // -  Any Repeated Keys 
+    // -  Underscores and Dashes trimming ; 
+    // - Any Email / Domain ..etc 'IGNORED'
+    // - Saving Translation ;
+    //     - For Edits and Combinig Then On Periods Of time 
+    // - Abbreviations 
+    // - 
  */
